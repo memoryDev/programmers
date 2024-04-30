9732ca1b0cfc345e970913f0ee9fff1cb454ceb6
memoryDev <godot4027@kakao.com>
2024-04-30 18:09:46 +0900

    코딩테스트 입문 > 배열 두 배 만들기

---
+++ src/입문/배열두배만들기/Solution.java
@@ -0,0 +1,16 @@
+package 입문.배열두배만들기;
+
+public class Solution {
+
+        public int[] solution (int[] numbers) {
+
+            int[] answer = new int[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                answer[i] = numbers[i] * 2;
+            }
+
+            return answer;
+        }
+
+}
