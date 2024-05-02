0fdd12658196daef7f3848fec8031166257f6284
memoryDev <godot4027@nate.com>
2024-05-02 18:03:48 +0900

    코딩테스트 입문 > 머쓱이보다 키 큰 사람

---
+++ src/입문/머쓱이보다키큰사람/Solution.java
@@ -0,0 +1,12 @@
+package 입문.머쓱이보다키큰사람;
+
+public class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > height) answer++;
+        }
+        return answer;
+    }
+}
