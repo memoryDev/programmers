34d30d6f60c4423c041c18d6dbde8df1d60e2c69
memoryDev <godot4027@nate.com>
2024-05-04 15:27:32 +0900

    코딩테스트 입문 > 아이스 아메리카노

---
+++ src/입문/아이스_아메리카노/Solution.java
@@ -0,0 +1,19 @@
+package 입문.아이스_아메리카노;
+
+public class Solution {
+    public int solution(int price) {
+        int answer = 0;
+
+        if( price >= 500000) {
+            answer = (int)(price*0.8);
+        } else if( price >= 300000){
+            answer = (int)(price * 0.9);
+        }else if (price >= 100000){
+            answer = (int)(price * 0.95);
+        } else {
+            answer = price;
+        }
+
+        return answer;
+    }
+}
